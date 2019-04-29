c575df6c6b30dab869ff017bef96f47dcc90446f
Sümeyye Keskin <smeyyekeskn@gmail.com>
2019-04-29 14:37:39 +0300

    Completed Project

--- PiWorksTechnicalQuestion3/Program.cs
+++ PiWorksTechnicalQuestion3/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace PiWorksTechnicalQuestion3
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            int number1 = 10;
+            int number2 = 20;
+
+            Console.WriteLine("Value before relocation");
+            Console.WriteLine("Number 1 = {0}", number1);
+            Console.WriteLine("Number 2 = {0}", number2);
+
+
+            number1 = number1 + number2;
+            number2 = number1 - number2;
+            number1 = number1 - number2;
+            Console.WriteLine("___________________________________");
+            Console.WriteLine("Value after relocation");
+            Console.WriteLine("Number 1 = {0}", number1);
+            Console.WriteLine("Number 2 = {0}", number2);
+
+            Console.ReadKey();
+
         }
     }
 }
